651d725a8e2c5c5117bdb2d58de263ba30b7c832
Mark Marsella <mmarsella114@g.rwu.edu>
2018-01-12 10:27:50 -0800

    day2 part2 complete

--- day2/part2.js
+++ day2/part2.js
@@ -2,45 +2,36 @@ var fs = require('fs');
 var puzzleInput = fs.readFileSync('puzzleInput.txt').toString();
 var arr = puzzleInput.split(' +');
 arr = arr[0].split('\n')
-
-
 var sample = [
 "5 9 2 8",
 "9 4 7 3",
 "3 8 6 5"
 ];
-
 var n = arr[0].split(' ');
 let sum = 0;
 
 // each space sep list of numbers
-sample.forEach(function(elem){
-
+arr.forEach(function(elem){
   let foundResult;
   let nums = elem.split(' ');  
   nums.forEach(function(el,i,arr){
-    console.log('el', el)
     el = parseInt(el);
     if(!Number.isInteger(el)){return;}
-
     for(let idx=i + 1; idx <= arr.length-1; idx++){
-      console.log('arr[idx]', arr[idx], el)
+      // try dividing #s in order
+      foundResult = el % arr[idx] === 0 ? el / arr[idx] : null;
 
-      let divider;
-      let operand;
-
-      if(el % arr[idx] === 0){
-        
-        foundResult = el > arr[idx] ? (el / arr[idx]) : (arr[idx] / el);
-        console.log('foundResult', foundResult)
+      // try dividing #s in reverse order
+      if(!foundResult){
+        foundResult = arr[idx] % el === 0 ? arr[idx] / el : null;
+      }
+      // Break out if we found a keeper
+      if(foundResult){
+        sum += foundResult || 0;
         break;
-      } 
+      }
     }
-
   })
-
-  sum += foundResult || 0;
 })
-
 
 console.log(`****** ${sum} ********`)
